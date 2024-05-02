2ca806230f655f8dede0ff74476c7a3f3790c17e
Sanjay A V <sanjayav1262@gmail.com>
2024-05-02 21:57:07 +0530

    Create VM outline

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]){
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
@@ -10,9 +12,11 @@ int main(int argc, const char* argv[]){
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_RETURN, 123);
+    writeChunk(&chunk, OP_RETURN, 124);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
